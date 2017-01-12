9580c6751b1fa3b3726c8ddefc00c9b96077b0fc
Eric Liu <ericliu314@gmail.com>
2017-01-12 18:06:15 -0500

    Updated styles

--- menu.js
+++ menu.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
+		$('#logo').toggleClass('open');
+		$('#topnav').toggleClass('open');
+	  $('#option1').toggleClass('open');
+		$('#option2').toggleClass('open');
+		$('#option3').toggleClass('open');
+
 	});
 });
