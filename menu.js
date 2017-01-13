45c8a04c89c8ab40da2cb09ec011f34fd88f5f20
Eric Liu <ericliu314@gmail.com>
2017-01-12 21:24:11 -0500

    Homepage

--- menu.js
+++ menu.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
 
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
+
 		$('#logo').toggleClass('open');
 		$('#topnav').toggleClass('open');
-	  $('#option1').toggleClass('open');
-		$('#option2').toggleClass('open');
-		$('#option3').toggleClass('open');
 
+		$('#menu-1').toggleClass('open');
+		$('#menu-2').toggleClass('open');
+		$('#menu-3').toggleClass('open');
+		$('#menu-4').toggleClass('open');
+
+		$('#opt1').toggleClass('open');
+		$('#opt2').toggleClass('open');
+		$('#opt3').toggleClass('open');
 	});
+	
 });
