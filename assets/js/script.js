1c5f8c133377af38903691bf645e44037cd40095
Eric Liu <ericliu314@gmail.com>
2017-04-03 13:19:32 -0400

    Nav bar transitions on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+	 var scroll_start = 0;
+   var startchange = $('.nav-scroll');
+   var offset = startchange.offset();
+
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav-scroll').css('background-color', '#fff');
+					$('.nav-scroll').css('box-shadow', '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)');
+       } else {
+          $('.nav-scroll').css('background-color', 'transparent');
+					$('.nav-scroll').css('box-shadow', 'none');
+       }
+   });
+
 
 	$('#nav-icon, #nav-icon-dark').click(function(){
 		$(this).toggleClass('open');
@@ -13,5 +28,4 @@ $(document).ready(function(){
 		$('#option-3').toggleClass('open');
 		$('#option-4').toggleClass('open');
 	});
-
 });
