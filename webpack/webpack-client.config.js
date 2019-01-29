61f33cb87fc28b9c28053164d34b9a24ebdd3585
Eric Liu <ericliu314@gmail.com>
2019-01-29 01:29:25 -0800

    Major refactor

--- webpack/webpack-client.config.js
+++ webpack/webpack-client.config.js
@@ -81,6 +81,16 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
+      },
     ],
   },
   plugins: [
