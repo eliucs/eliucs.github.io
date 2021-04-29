f5f5e60369ab02f5a6fb7550382b8ca135564197
Eric Liu <ericliu314@gmail.com>
2021-04-29 15:36:23 -0700

    add important notice

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import Config from 'configuration';
 import InteractiveText from 'components/InteractiveText';
@@ -43,6 +44,17 @@ const HomePage = ({ dispatch }) => {
           <HomeLinks
             profile={Config.get('profileCard')}
           />
+
+          <Modal isOpen size="lg">
+            <ModalHeader>Important</ModalHeader>
+            <ModalBody>
+              <p>It has come to my attention that a student named &quot;Eric Liu&quot; is being mentioned on the r/uwaterloo subreddit.</p>
+              <p>That person is <b>NOT</b> me.</p>
+              <p>They just happen to have the same common name. I&apos;ve already graduated in 2020.</p>
+              <p>Please do not direct any harassment towards me, my friends or the other students named &quot;Eric Liu&quot; at Waterloo who are not involved.</p>
+              <p>Thank you.</p>
+            </ModalBody>
+          </Modal>
         </div>
       </div>
     </div>
